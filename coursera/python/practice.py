faec1b99771cb1cfdfc2813bf2401088dde7781f
Clif Hudson <clifrunsalot@yahoo.com>
2017-12-12 05:04:59 -0500

    Add python folder

---
+++ coursera/python/practice.py
@@ -0,0 +1,21 @@
+"""
+Demonstration of simple arithmetic expression in Python.
+"""
+
+# Unary + and -
+print("Unary operators")
+print(+3)
+print(-5)
+print(+7.86)
+print(-3348.63)
+
+print("")
+
+# Simple arithmetic
+print("Addition and Subtraction")
+print(1 + 2)
+print(48 - 89)
+print(3.45 + 2.7)
+print(87.3384 - 12.35)
+print(3 + 6.7)
+print(9.8 - 4)
