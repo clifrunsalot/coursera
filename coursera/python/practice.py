49dfc70e6a30ab56fc34ea9d3f261f4949be6cf7
Clif Hudson <clifrunsalot@yahoo.com>
2017-12-28 07:32:01 -0500

    week 2 quiz notes

--- coursera/python/practice.py
+++ coursera/python/practice.py
@@ -81,4 +81,61 @@ miles = 5
 total_ft = 5 * 5280
 print(str(total_ft) + " ft in " + str(miles) + " miles")
 
+############# quiz practice ##############
+
+def max_of_2(val1, val2):
+    if val1 > val2:
+        return val1
+    else:
+        return val2
+
+def max_of_3(val1, val2, val3):
+    return max_of_2(val1, max_of_2(val2, val3))
+
+#########################
+
+def project_to_distance(point_x, point_y, distance):
+    dist_to_origin = (point_x ** 2 + point_y ** 2) ** 0.5
+    scale = distance / dist_to_origin
+    print(point_x * scale, point_y * scale)
+
+project_to_distance(2, 7, 4)
+
+#########################
+
+def funcX(val):
+  return (-5*(val**5)) + (67*(val**2)) - 47
+  
+print(funcX(0))
+print(funcX(1))
+print(funcX(2))
+print(funcX(3))
+
+##########################
+
+def future_value(present_value, annual_rate, periods_per_year, years):
+    """
+    Input: the numbers present_value, annual_rate, periods_per_year, years
+    Output: future value based on formula given in question
+    """
+    rate_per_period = annual_rate / periods_per_year
+    periods = periods_per_year * years
+
+    # Put your code here.
+    return (present_value * ((1 + rate_per_period) ** periods))
+    
+
+print("$1000 at 2% compounded daily for 4 years yields $", future_value(1000, .02, 365, 4))
+print("$500 at 4% compounded 10 times per year for 10 years yields $", future_value(500, .04, 10, 10))
+
+########################
+
+def EqTriArea(side):
+  return ((3**0.5) / 4) * (side**2)
+  
+print(EqTriArea(1))
+print(EqTriArea(2))
+print(EqTriArea(3))
+print(EqTriArea(4))
+print(EqTriArea(5))
 
